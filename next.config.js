1ce7d8a8d463f8f5fb4fb9854cff74e13ce8b134
safsafsaasfsaf26-crypto <safsafsaasfsaf26@gmail.com>
2025-08-02 21:15:34 +0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [], // Add image domains here if needed
-  },
-};
-
-module.exports = nextConfig;
